a6fa45d16e51ba7262de80c65adbabfb710369dc
daasc <paulojhol@gmail.com>
2022-03-09 21:30:53 -0300

    add store and all test of the flash store

---
+++ store/flash.js
@@ -0,0 +1,53 @@
+export const state = () => ({
+  result: 0,
+  multiplier: 0,
+  secondMultiplier: 0,
+  answer: [],
+  isCorrect: null,
+})
+
+export const mutations = {
+  SET_MULTIPLIER: (state) => {
+    state.multiplier = Math.floor(Math.random() * 100) + 1
+  },
+  SET_SECOND_MULTIPLIER: (state) => {
+    state.secondMultiplier = Math.floor(Math.random() * 100) + 1
+  },
+  SET_RESULT: (state) => {
+    state.result = state.multiplier * state.secondMultiplier
+  },
+  SET_ANSWER: (state) => {
+    state.answer.push(
+      state.multiplier * state.secondMultiplier + state.multiplier
+    )
+    state.answer.push(
+      state.multiplier * state.secondMultiplier + state.secondMultiplier
+    )
+    state.answer.push(
+      state.multiplier * state.secondMultiplier - Math.floor(Math.random() * 5)
+    )
+    state.answer.push(
+      state.multiplier * state.secondMultiplier + Math.floor(Math.random() * 5)
+    )
+    state.answer.push(state.multiplier * state.secondMultiplier + 6)
+    state.answer.push(state.result)
+  },
+  CHECK_RESULT: (state, payload) => {
+    if (state.result === payload) {
+      state.isCorrect = true
+    } else {
+      state.isCorrect = false
+    }
+  },
+  AGAIN: (state) => {
+    mutations.SET_MULTIPLIER(state)
+    mutations.SET_SECOND_MULTIPLIER(state)
+    mutations.SET_RESULT(state)
+    mutations.SET_ANSWER(state)
+    state.isCorrect = null
+  },
+}
+
+export const getters = {}
+
+export const actions = {}
